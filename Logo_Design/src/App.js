416d907ff30d5da38f8c6d6a4b3f9c53bb02c9c8
prempk-tech <74701984+prempk-tech@users.noreply.github.com>
2022-12-04 10:02:27 +0530

    logo

---
+++ Logo_Design/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Route from './Components/logo';
+
+
+function App() {
+  return (
+    <div>
+      <Route />
+    </div>
+  );
+}
+
+export default App;
---
+++ Logo_Design/src/Components/logo.js
@@ -0,0 +1,27 @@
+import React from "react";
+import '../Styles/logo.css'
+class Route extends React.Component{
+    render(){
+        return(
+            <div>
+                <div className="Finalbox">
+                <div className="container"> 
+                   <div className="div div1">
+                    
+                   <div className="box1 ">
+                   <div className="box2 ">
+                   <div className="box3 ">
+                   <div className="box4 ">
+                   <div className="text">HTML<span>&</span>CSS<br></br>design and build website</div>
+                        </div>
+                       </div>
+                      </div>
+                     </div>
+                    </div> 
+                   </div>
+                </div>       
+            </div>
+        )
+    }
+}
+export default Route;
